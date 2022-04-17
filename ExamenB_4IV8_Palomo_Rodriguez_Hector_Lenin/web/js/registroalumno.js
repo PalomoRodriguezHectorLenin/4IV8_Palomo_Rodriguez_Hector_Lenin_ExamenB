f4667af21a940d145a1af52e798353966e07d31d
PalomoRodriguezHectorLenin <hpalomor2000@alumno.ipn.mx>
2022-04-17 16:03:31 -0500

    CambiosEnRegistroYJs

---
+++ ExamenB_4IV8_Palomo_Rodriguez_Hector_Lenin/web/js/registroalumno.js
@@ -0,0 +1,9 @@
+
+function validarDatos(){
+    var boleta = document.getElementById("boleta");
+    var nombre = document.getElementById("name");
+    var primerApellido = document.getElementById("apellido1");
+    var segundoApellido = document.getElementById("apellido2");
+    var semestre = document.getElementById("semestre");
+    var turno = document.getElementById("turno");
+}
